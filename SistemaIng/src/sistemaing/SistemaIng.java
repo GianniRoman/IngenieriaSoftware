2f885cf630ab4442992d52b0cd2402ee0c31ab3c
GianniRoman <g.lupic@hotmail.com>
2017-05-06 12:29:09 -0300

    Cambios2

--- SistemaIng/src/sistemaing/SistemaIng.java
+++ SistemaIng/src/sistemaing/SistemaIng.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemaing;
+import Modelos.ConexionBD;
 
-/**
- *
- * @author glupi
- */
 public class SistemaIng {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
